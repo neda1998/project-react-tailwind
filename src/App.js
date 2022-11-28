83f9aaeb1ef6b991f7c2cf98537c248b00f7b85a
neda1998 <nedagholami1998@gmail.com>
2022-11-28 19:41:13 +0330

    added new components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+
+function App() {
+ return (
+    <div>
+        <Navbar />
+        <Hero />
+    </div>
+ )
+}
+
+export default App;
